92609d87ebc8aad4e2f227e2d363861c27bed6ad
weiguo.ni <neefrankie@gmail.com>
2021-10-26 17:33:32 +0800

    refactor: moved alert to parent

--- src/components/forms/MobileLoginForm.tsx
+++ src/components/forms/MobileLoginForm.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Form, Formik, FormikHelpers } from 'formik';
-import Alert from 'react-bootstrap/Alert';
 import { invalidMessages, VerifySMSFormVal } from '../../data/form-value';
 import * as Yup from 'yup';
 import { TextInput } from '../controls/TextInput';
@@ -17,7 +16,6 @@ export interface SMSHelper {
 
 export function MobileLoginForm(
   props: {
-    errMsg: string;
     onSubmit: (
       values: VerifySMSFormVal,
       formikHelpers: FormikHelpers<VerifySMSFormVal>
@@ -33,103 +31,91 @@ export function MobileLoginForm(
   const [showCounter, setShowCounter] = useState(false);
 
   return (
-    <>
-      {
-        props.errMsg &&
-        <Alert
-          variant="danger"
-          dismissible
-          onClose={() => props.errMsg = ''}
-        >
-          {props.errMsg}
-        </Alert>
-      }
-      <Formik<VerifySMSFormVal>
-        initialValues={{
-          mobile: '',
-          code: '',
-        }}
-        validationSchema={Yup.object({
-          mobile: Yup.string()
-            .matches(/^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\d{8}$/, '请输入正确的手机号')
-            .required(invalidMessages.required),
-          code: Yup.string()
-            .required(invalidMessages.required),
-        })}
-        onSubmit={props.onSubmit}
-      >
-        {formik => (
-          <Form>
-            <TextInput
-              label="手机号码"
-              name="mobile"
-              type="text"
-              desc="仅限中国大陆地区"
-            />
+    <Formik<VerifySMSFormVal>
+      initialValues={{
+        mobile: '',
+        code: '',
+      }}
+      validationSchema={Yup.object({
+        mobile: Yup.string()
+          // .matches(/^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\d{8}$/, '请输入正确的手机号')
+          .required(invalidMessages.required),
+        code: Yup.string()
+          .required(invalidMessages.required),
+      })}
+      onSubmit={props.onSubmit}
+    >
+      {formik => (
+        <Form>
+          <TextInput
+            label="手机号码"
+            name="mobile"
+            type="text"
+            desc="仅限中国大陆地区"
+          />
 
-            <div  className="mb-3">
-              <label
-                className="form-label"
-                htmlFor="code"
-              >
-                验证码
-              </label>
+          <div  className="mb-3">
+            <label
+              className="form-label"
+              htmlFor="code"
+            >
+              验证码
+            </label>
 
-              <InputGroup>
-                <FormControl
-                  type="text"
-                  id="code"
-                  name="code"
-                  onInput={formik.handleChange}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.code}
-                />
+            <InputGroup>
+              <FormControl
+                type="text"
+                id="code"
+                name="code"
+                onInput={formik.handleChange}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.code}
+              />
 
-                {
-                  showCounter ?
-                  <CounterButton
-                    variant="outline-secondary"
-                    from={60}
-                    onFinish={() => setShowCounter(false)}
-                  /> :
-                  <ProgressButton
-                    disabled={!(formik.touched.mobile && !formik.errors.mobile) || formik.isSubmitting}
-                    text="获取"
-                    isSubmitting={requestingSMS}
-                    variant="outline-secondary"
-                    styleClass={styles.counter}
-                    asButton={true}
-                    inline={true}
-                    onClick={() => {
-                      props.onRequestSMS(
-                        formik.values.mobile,
-                        {
-                          setProgress: setRequestingSMS,
-                          setShowCounter: setShowCounter,
-                        }
-                      )
-                    }}
-                  />
-                }
-              </InputGroup>
               {
-                formik.errors.mobile ? (
-                  <div className="invalid-feedback">
-                    {formik.errors.mobile}
-                  </div>
-                ) : null
+                showCounter ?
+                <CounterButton
+                  variant="outline-secondary"
+                  from={60}
+                  onFinish={() => setShowCounter(false)}
+                /> :
+                <ProgressButton
+                  disabled={!(formik.touched.mobile && !formik.errors.mobile) || formik.isSubmitting}
+                  text="获取"
+                  isSubmitting={requestingSMS}
+                  variant="outline-secondary"
+                  styleClass={styles.counter}
+                  asButton={true}
+                  inline={true}
+                  onClick={() => {
+                    props.onRequestSMS(
+                      formik.values.mobile,
+                      {
+                        setProgress: setRequestingSMS,
+                        setShowCounter: setShowCounter,
+                      }
+                    )
+                  }}
+                />
               }
-            </div>
+            </InputGroup>
+            {
+              formik.errors.mobile ? (
+                <div className="invalid-feedback">
+                  {formik.errors.mobile}
+                </div>
+              ) : null
+            }
+          </div>
 
-            <ProgressButton
-              disabled={!(formik.dirty && formik.isValid) || formik.isSubmitting}
-              text="登录"
-              isSubmitting={formik.isSubmitting}
-            />
-          </Form>
-        )}
-      </Formik>
-    </>
+          <ProgressButton
+            disabled={!(formik.dirty && formik.isValid) || formik.isSubmitting}
+            text="登录"
+            isSubmitting={formik.isSubmitting}
+          />
+        </Form>
+      )}
+    </Formik>
   );
 }
