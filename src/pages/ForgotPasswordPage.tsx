dd96811d3c23aefa6b653c7e83efe51e42eb9f3a
weiguo.ni <neefrankie@gmail.com>
2021-10-28 17:19:42 +0800

    refactor: password reset

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -1,21 +1,77 @@
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { FormikHelpers } from 'formik';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { RequestPwResetForm } from '../components/forms/RequestPwResetForm';
 import { CenterLayout } from '../components/Layout';
-import { PasswordReset } from './auth/forgot-password/PasswordReset';
-import { RequestPasswordReset } from './auth/forgot-password/ReqeustPasswordReset';
+import { EmailFormVal } from '../data/form-value';
+import { passwordResetUrl, sitemap } from '../data/sitemap';
+import { requestPasswordReset } from '../repository/auth';
+import { ResponseError } from '../repository/response-error';
 
-export function ForgotPasswordPage() {
-  let match = useRouteMatch();
+function RequestPwResetLetter(
+  props: {
+    baseUrl: string;
+  }
+) {
+  const [ done, setDone ] = useState(false);
+  const [ errMsg, setErrMsg ] = useState('');
+
+  if (done) {
+    return (
+      <div>
+        <span>请检查您的邮件，点击邮件中的“重置密码”按钮修改您的密码。如果几分钟内没有看到邮件，请检查是否被放进了垃圾邮件列表。</span>
+        <div className="d-grid mt-3">
+          <Link to={sitemap.login} className="btn btn-primary">返回</Link>
+        </div>
+      </div>
+    );
+  }
+
+  const handleSubmit = (
+    values: EmailFormVal,
+    helper: FormikHelpers<EmailFormVal>
+  ): void | Promise<any> => {
+    setErrMsg('');
+
+    helper.setSubmitting(true);
+
+    requestPasswordReset({
+      ...values,
+      sourceUrl: passwordResetUrl(props.baseUrl),
+    })
+      .then(ok => {
+        helper.setSubmitting(!ok);
+        setDone(ok);
+      })
+      .catch((err: ResponseError) => {
+        helper.setSubmitting(false);
+        if (err.invalid) {
+          helper.setErrors(err.toFormFields);
+          return;
+        }
+        setErrMsg(err.message);
+      });
+  }
 
   return (
+    <RequestPwResetForm
+      errMsg={errMsg}
+      onSubmit={handleSubmit}
+    />
+  );
+}
+
+export function ForgotPasswordPage() {
+
+  console.log(document.location);
+  return (
     <CenterLayout>
-      <Switch>
-        <Route path={`${match.url}/:token`}>
-          <PasswordReset />
-        </Route>
-        <Route path={match.url}>
-          <RequestPasswordReset />
-        </Route>
-      </Switch>
+      <>
+        <h2 className="text-center">找回密码</h2>
+        <RequestPwResetLetter
+          baseUrl={document.location.origin}
+        />
+      </>
     </CenterLayout>
   );
 }
