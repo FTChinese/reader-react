27817a0801b10151f3d636f9285c5919f2ff2561
weiguo.ni <neefrankie@gmail.com>
2021-10-28 11:28:36 +0800

    feat: add membership path

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ const navItems: ILink[] = [
   },
   {
     name: '会员订阅',
-    href: sitemap.subs,
+    href: sitemap.membership,
   },
 ];
 
--- src/data/sitemap.ts
+++ src/data/sitemap.ts
@@ -4,6 +4,7 @@ export const sitemap = {
   signUp: '/signup',
   passwordReset: '/password-reset', // Show email input box, or perform verification if there's a url parameter following this path.
   verification: '/verification',
+  membership: '/membership',
   subs: '/subscription',
   products: '/products',
   settings: '/settings',
