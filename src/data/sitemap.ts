9473b5f9a55570eccf57eabd44df7e30a90ba439
weiguo.ni <neefrankie@gmail.com>
2021-10-28 11:03:37 +0800

    refactor: password-reset path

--- src/data/sitemap.ts
+++ src/data/sitemap.ts
@@ -2,7 +2,7 @@ export const sitemap = {
   home: '/',
   login: '/login',
   signUp: '/signup',
-  forgotPassword: '/forgot-password',
+  passwordReset: '/password-reset', // Show email input box, or perform verification if there's a url parameter following this path.
   verification: '/verification',
   subs: '/subscription',
   products: '/products',
