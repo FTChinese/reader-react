a74524c0ffdf53dbc181a3819dc0d00f927f6468
weiguo.ni <neefrankie@gmail.com>
2021-10-28 11:28:43 +0800

    refactor

---
+++ src/components/product/CustomerSerivce.tsx
@@ -0,0 +1,14 @@
+export function CustomerService() {
+  return (
+    <div className="mt-3">
+      <h6>联系客服</h6>
+      <dl>
+        <dt>电话</dt>
+        <dd>010-xxxxxx</dd>
+
+        <dt>邮箱</dt>
+        <dd><a href="mailto:subscriber.service@ftchinese.com">subscriber.service@ftchinese.com</a></dd>
+      </dl>
+    </div>
+  );
+}
