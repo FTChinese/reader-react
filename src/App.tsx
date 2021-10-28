4877ff9167342dc5640adba856329c4cc413f902
weiguo.ni <neefrankie@gmail.com>
2021-10-28 11:08:10 +0800

    refactor: MembershipPage

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './store/AuthContext';
 import { LoginPage } from './pages/LoginPage';
 import { SignUpPage } from './pages/SignUpPage';
 import { ForgotPasswordPage } from './pages/auth/ForgotPasswordPage';
-import { ProductPage } from './pages/product/ProductPage';
+import { ProductPage } from './pages/MembershipPage';
 import { HomePage } from './pages/HomePage';
 import { AuthRoute, ProtectedRoute } from './components/routes/ProtectedRoute';
 import { sitemap } from './data/sitemap';
--- src/pages/MembershipPage.tsx
+++ src/pages/MembershipPage.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { localizedCycle } from '../../data/localization';
-import { Paywall, Price, ProductGroup } from '../../data/paywall';
-import { paywallRepo } from '../../repository/paywall';
-import { useAuthContext } from '../../store/AuthContext';
-import { Unauthorized } from '../../components/routes/Unauthorized';
-import { ResponseError } from '../../repository/response-error';
-import { ContentLayout } from '../../components/Layout';
+import { localizedCycle } from '../data/localization';
+import { Paywall, Price, ProductGroup } from '../data/paywall';
+import { paywallRepo } from '../repository/paywall';
+import { useAuthContext } from '../store/AuthContext';
+import { Unauthorized } from '../components/routes/Unauthorized';
+import { ResponseError } from '../repository/response-error';
+import { ContentLayout } from '../components/Layout';
 
 interface PriceProps {
   price: Price;
