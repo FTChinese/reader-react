74fc3ea7ec30db8e756c17e0293eea5a2653fab1
weiguo.ni <neefrankie@gmail.com>
2021-11-05 17:28:53 +0800

    feat: show mobile form

--- src/components/account/MobileRow.tsx
+++ src/components/account/MobileRow.tsx
@@ -1,4 +1,7 @@
+import { FormikHelpers } from 'formik';
 import { useState } from 'react';
+import { VerifySMSFormVal } from '../../data/form-value';
+import { MobileLoginForm, SMSHelper } from '../forms/MobileLoginForm';
 import { AccountRow } from "./AccountRow";
 
 export function DisplayMobile(
@@ -10,13 +13,39 @@ export function DisplayMobile(
   const [editing, setEditing] = useState(false);
   const [errMsg, setErrMsg] = useState('');
 
+  const handleSMSRequest = (mobile: string, helper: SMSHelper) => {
+    console.log(mobile);
+    helper.setProgress(true);
+
+    console.log(mobile);
+  };
+
+  const handleSubmit = (
+    values: VerifySMSFormVal,
+    helper: FormikHelpers<VerifySMSFormVal>
+  ): void | Promise<any> => {
+    setErrMsg('');
+    helper.setSubmitting(true);
+
+    console.log(values)
+  }
+
   return (
     <AccountRow
       title="手机号"
       isEditing={editing}
       onEdit={() => setEditing(!editing)}
     >
-      <div>{props.mobile || '未设置'}</div>
+      { editing ?
+        <MobileLoginForm
+          onSubmit={handleSubmit}
+          onRequestSMS={handleSMSRequest}
+          errMsg={errMsg}
+          isLogin={false}
+        /> :
+
+        <div>{props.mobile || '未设置'}</div>
+      }
     </AccountRow>
   );
 }
