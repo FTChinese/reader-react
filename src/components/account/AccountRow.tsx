594e53179f76f6eeb66feba912f454c13a39f8e5
weiguo.ni <neefrankie@gmail.com>
2021-11-05 16:25:51 +0800

    feat: edit or cancel icon

--- src/components/account/AccountRow.tsx
+++ src/components/account/AccountRow.tsx
@@ -1,12 +1,31 @@
+import { XLarge, Pencil } from '../icons';
+
 export function AccountRow(
   props: {
     title: string;
     children: JSX.Element;
+    isEditing?: boolean;
+    onEdit?: () => void
   }
 ) {
+
   return (
     <div className="border-bottom pb-3 pt-3">
-      <h5>{props.title}</h5>
+      <div className="d-flex justify-content-between align-items-center">
+        <h5>{props.title}</h5>
+
+        {!props.onEdit ||
+          <button className="btn btn-link"
+            onClick={props.onEdit}
+          >
+            { props.isEditing ?
+              <XLarge /> :
+              <Pencil />
+            }
+          </button>
+        }
+      </div>
+
       {props.children}
     </div>
   );
