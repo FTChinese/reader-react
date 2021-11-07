522924b67eb182a607a7030d7d640731e454fa6f
weiguo.ni <neefrankie@gmail.com>
2021-11-07 23:26:28 +0800

    feat: handle localStorage in a central palce

---
+++ src/store/keys.ts
@@ -0,0 +1,32 @@
+import { WxOAuthCodeReq, WxOAuthCodeSession } from '../data/authentication';
+import { WxOAuthKind } from '../data/enum';
+import { unixNow } from '../utils/now';
+
+export const storeKeyAccount = 'fta-reader';
+const storeKeyWxLogin = 'fta-wx-oauth'
+
+export const wxOAuthState = {
+
+  save(req: WxOAuthCodeReq, k: WxOAuthKind) {
+    const sess: WxOAuthCodeSession = {
+      ...req,
+      expiresAt: unixNow() + 5 * 60,
+      kind: k,
+    }
+
+    localStorage.setItem(storeKeyWxLogin, JSON.stringify(sess));
+  },
+
+  load(): WxOAuthCodeSession | null {
+    const v = localStorage.getItem(storeKeyWxLogin);
+    if (!v) {
+      return null;
+    }
+
+    return JSON.parse(v) as WxOAuthCodeSession;
+  },
+
+  remove() {
+    localStorage.removeItem(storeKeyWxLogin);
+  }
+}
