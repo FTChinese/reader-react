8dc3b3c716450b0183e7f8fa40a9feab4abc91c3
weiguo.ni <neefrankie@gmail.com>
2021-11-07 09:32:33 +0800

    feat: wx oauth endpoints

--- src/repository/endpoint.ts
+++ src/repository/endpoint.ts
@@ -3,6 +3,7 @@ const readerBase = '/api/reader';
 
 const emailAuthBase = `${readerBase}/auth/email`;
 const mobileAuthBase = `${readerBase}/auth/mobile`;
+const wxAuthBase = `${readerBase}/auth/wx`;
 const pwResetBase = `${readerBase}/auth/password-reset`;
 const accountBase = `${readerBase}/account`;
 
@@ -21,6 +22,10 @@ export const endpoint = {
   },
   resetPassword: `${pwResetBase}`,
   emailAccount: `${accountBase}`,
+
+  wxCode: `${wxAuthBase}/code`,
+  wxLogin: `${wxAuthBase}/login`,
+  wxRefresh: `${wxAuthBase}/refresh`,
 
   // Update account fields
   changeEmail: `${accountBase}/email`,
