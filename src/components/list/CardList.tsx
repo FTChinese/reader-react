c8504ba9fb802543c2d4d33fb3ab131e88d59d8e
weiguo.ni <neefrankie@gmail.com>
2021-11-09 15:19:29 +0800

    feat: CartList and TwoColList

---
+++ src/components/list/CardList.tsx
@@ -0,0 +1,32 @@
+import { StringPair } from './pair';
+import { TwoColList } from './TwoColList';
+
+export function CardList(
+  props: {
+    rows: StringPair[];
+    header?: string;
+    title?: string;
+    children?: JSX.Element;
+    className?: string;
+  }
+) {
+  const wrapperClass = props.className ? `card ${props.className}` : 'card';
+
+  return (
+    <div className={wrapperClass}>
+      {
+        props.header &&
+        <div className="card-header text-muted text-center">{props.header}</div>
+      }
+      <div className="card-body">
+        {
+          props.title &&
+          <h5 className="card-title">{props.title}</h5>
+        }
+        {props.children}
+      </div>
+
+      <TwoColList rows={props.rows}/>
+    </div>
+  );
+}
