e9e0a7320430509a2db43d40bebdbf80134f8c12
neefrankie <neefrankie@gmail.com>
2021-11-09 23:57:37 +0800

    refactor: rename and relocate SubsDetails

--- src/pages/MembershipPage.tsx
+++ src/pages/MembershipPage.tsx
@@ -1,34 +1,9 @@
 import { useAuthContext } from '../store/AuthContext';
 import { ContentLayout } from '../components/Layout';
-import { isMembershipZero, Membership } from '../data/membership';
 import { Redirect } from 'react-router-dom';
 import { sitemap } from '../data/sitemap';
-import { buildMemberStatus } from '../data/member-status';
-import { CardList } from '../components/list/CardList';
+import { CurrentSubs } from '../components/subs/CurrentSubs';
 
-function SubsDetail(
-  props: Membership
-) {
-  if (isMembershipZero(props)) {
-    return <div>您尚未订阅FT中文网服务。去订阅</div>;
-  }
-
-  const memberStatus = buildMemberStatus(props);
-
-  return (
-    <CardList
-      rows={memberStatus.details}
-      header="当前订阅"
-      title={memberStatus.productName}
-    >
-      {
-        memberStatus.reminder ?
-        <p className="text-danger text-center">{memberStatus.reminder}</p> :
-        <></>
-      }
-    </CardList>
-  )
-}
 
 export function MembershipPage() {
   const { passport } = useAuthContext();
@@ -40,7 +15,7 @@ export function MembershipPage() {
     <ContentLayout>
       <div className="row justify-content-center">
         <div className="col-md-8 col-lg-6">
-          <SubsDetail
+          <CurrentSubs
             {...passport.membership}
           />
         </div>
