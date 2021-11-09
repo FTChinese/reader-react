ac003a8619adfa3a76bbf3f4395b1846f1fc3562
neefrankie <neefrankie@gmail.com>
2021-11-09 23:55:49 +0800

    refactor: current subs ui building

--- src/data/member-status.ts
+++ src/data/member-status.ts
@@ -1,25 +1,18 @@
-import { differenceInDays, getDate, getMonth, parseISO, startOfDay } from 'date-fns';
+import { getDate, getMonth, parseISO } from 'date-fns';
 import { StringPair } from '../components/list/pair';
-import { isExpired } from '../utils/now';
-import { Cycle, isInvalidSubStatus, SubStatus } from './enum';
+import { diffToday, isExpired } from '../utils/now';
+import { Cycle, isInvalidSubStatus, PaymentMethod, SubStatus } from './enum';
 import { localizedCycle, localizedTier, localizePaymentMethod } from './localization';
 import { Membership } from './membership';
 
 /**
  * @description Describes the UI used to present Membership.
  */
- export type MemberStatus = {
-  reminder?: string;
+export type MemberStatus = {
   productName: string;
   details: StringPair[];
+  reminder?: string;
   reactivateStripe?: boolean;
-}
-
-const expirationTitle = '到期时间';
-
-function getRemainingDays(date: Date): number {
-  const today = startOfDay(new Date());
-  return differenceInDays(date, today);
 }
 
 function formatRemainingDays(expiresAt?: Date, subStatus?: SubStatus | null): string | undefined {
@@ -27,7 +20,7 @@ function formatRemainingDays(expiresAt?: Date, subStatus?: SubStatus | null): st
     return undefined;
   }
 
-  const n = getRemainingDays(expiresAt);
+  const n = diffToday(expiresAt);
 
   if (n < 0) {
     return '会员已过期';
@@ -49,11 +42,11 @@ function formatRemainingDays(expiresAt?: Date, subStatus?: SubStatus | null): st
 }
 
 
-function formatAutoRenewMoment(expiresAt?: Date, cycle?: Cycle): string {
-  if (!expiresAt || !cycle) {
-    return ''
-  }
-
+/**
+ * @description Show auto renewal date
+ * @example 11月11日/年 or 11日/月 depending on the value of cycle.
+ */
+function formatAutoRenewMoment(expiresAt: Date, cycle: Cycle): string {
   switch (cycle) {
     case 'year':
       return `${getMonth(expiresAt)}月${getDate(expiresAt)}日/${localizedCycle(cycle)}`
@@ -63,22 +56,43 @@ function formatAutoRenewMoment(expiresAt?: Date, cycle?: Cycle): string {
   }
 }
 
-const vipSubsStatus: MemberStatus = {
-  productName: '超级会员',
-    details: [
-      [expirationTitle, '无限期']
-    ],
-};
+function rowSubsSource(pm: PaymentMethod | null): StringPair {
+  return ['订阅方式', localizePaymentMethod(pm)];
+}
 
+function rowExpiration(date: string | null, isVip: boolean = false): StringPair {
+  return [
+    '到期时间',
+    isVip ? '无限期' : (date || '-')
+  ];
+}
+
+function rowAutoRenewDate(expiresAt: Date, cycle: Cycle): StringPair {
+  return ['自动续订', formatAutoRenewMoment(expiresAt, cycle)];
+}
+
+function rowAutoRenewOn(): StringPair {
+  return ['自动续订', '已开启'];
+}
+
+function rowAutoRenewOff(): StringPair {
+  return ['自动续订', '已关闭'];
+}
+
+/**
+ * @description Build card like:
+ *        标准会员
+ * 到期时间       2021-11-11
+ */
 function onetimeSubsStatus(m: Membership): MemberStatus {
   return {
+    productName: m.tier ? localizedTier(m.tier) : '',
+    details: [
+      rowExpiration(m.expireDate),
+    ],
     reminder: m.expireDate
       ? formatRemainingDays(parseISO(m.expireDate))
       : undefined,
-    productName: m.tier ? localizedTier(m.tier) : '',
-    details: [
-      [expirationTitle, m.expireDate || '未知'],
-    ],
   };
 }
 
@@ -90,37 +104,55 @@ function onetimeSubsStatus(m: Membership): MemberStatus {
  */
 function b2bMemberStatus(m: Membership): MemberStatus {
   return {
-    reminder: '企业订阅续订或升级请联系所属机构的管理人员',
     productName: m.tier ? localizedTier(m.tier) : '',
     details: [
-      ['订阅方式', localizePaymentMethod(m.payMethod)],
-      [expirationTitle, m.expireDate || '未知'],
+      rowSubsSource(m.payMethod),
+      rowExpiration(m.expireDate),
     ],
+    reminder: '企业订阅续订或升级请联系所属机构的管理人员',
   };
 }
 
 /**
- * @description Build a card like:
+ * @description Build a card for stripe or apple like:
  *      标准会员
- * 订阅方式     Stripe订阅
+ * 订阅方式     Stripe订阅 | 苹果App内购
  * 自动续订     xx月xx日/年
- * if auto renew if off
+ *
+ * or if either expireDate or cycle is missing:
  *      标准会员
- * 订阅方式     Stripe订阅
+ * 订阅方式     Stripe订阅 | 苹果App内购
+ * 自动续订     已开启
  * 到期时间     2021-11-11
+ *
+ * or if auto renew is off
+ *      标准会员
+ * 订阅方式     Stripe订阅 | 苹果App内购
  * 自动续订     已关闭
+ * 到期时间     2021-11-11
  */
-function stripeSubsStatus(m: Membership): MemberStatus {
+function autoRenewalSubsStatus(m: Membership): MemberStatus {
   const productName = m.tier ? localizedTier(m.tier) : '';
 
   const expiresAt = m.expireDate ? parseISO(m.expireDate) : undefined;
 
   if (m.autoRenew) {
+    if (!expiresAt || !m.cycle) {
+      return {
+        productName,
+        details: [
+          rowSubsSource(m.payMethod),
+          rowAutoRenewOn(),
+          rowExpiration(m.expireDate)
+        ]
+      };
+    }
+
     return {
       productName,
       details: [
-        ['订阅方式', 'Stripe订阅'],
-        ['自动续订', formatAutoRenewMoment(expiresAt, m.cycle)]
+        rowSubsSource(m.payMethod),
+        rowAutoRenewDate(expiresAt, m.cycle),
       ]
     };
   }
@@ -128,47 +160,35 @@ function stripeSubsStatus(m: Membership): MemberStatus {
   const expired = expiresAt ? isExpired(expiresAt) : false;
 
   return {
-    reminder: formatRemainingDays(expiresAt, m.status),
     productName,
     details: [
-      ['订阅方式', 'Stripe订阅'],
-      ['到期时间', m.expireDate || '未知'],
-      ['自动续订', '已关闭'],
+      rowSubsSource(m.payMethod),
+      rowAutoRenewOff(),
+      rowExpiration(m.expireDate),
     ],
-    reactivateStripe: !expired, // If auto renew is off and expiration date is not past.
+    reminder: formatRemainingDays(expiresAt, m.status),
+    // For stripe, if auto renew is off and expiration date is not past.
+    // You could only reactivate stripe subscripiton if
+    // if is not auto renewal and not expired yet.
+    // After expiration, this subscription is gone and to
+    // re-subscribe, you should create a new subscription.
+    reactivateStripe: (m.payMethod === 'stripe') && !expired,
   };
 }
 
-function appleMemberStatus(m: Membership): MemberStatus {
-  const payMethod = '苹果App内购';
-  const productName = m.tier ? localizedTier(m.tier) : '';
-
-  const expiresAt = m.expireDate ? parseISO(m.expireDate) : undefined;
-
-  if (m.autoRenew) {
-    return {
-      productName,
-      details: [
-        ['订阅方式', payMethod],
-        ['自动续订', formatAutoRenewMoment(expiresAt, m.cycle)]
-      ]
-    };
-  }
-
-  return {
-    reminder: formatRemainingDays(expiresAt, m.status),
-    productName,
-    details: [
-      ['订阅方式', 'Stripe订阅'],
-      ['到期时间', m.expireDate || '未知'],
-      ['自动续订', '已关闭'],
-    ],
-  };
-}
 
 export function buildMemberStatus(m: Membership): MemberStatus {
+  /**
+   *      超级会员
+   * 到期时间    无限期
+   */
   if (m.vip) {
-    return vipSubsStatus;
+    return {
+      productName: '超级会员',
+      details: [
+        rowExpiration(null, true)
+      ],
+    };
   }
 
   switch (m.payMethod) {
@@ -177,10 +197,8 @@ export function buildMemberStatus(m: Membership): MemberStatus {
       return onetimeSubsStatus(m);
 
     case 'stripe':
-      return stripeSubsStatus(m);
-
     case 'apple':
-      return appleMemberStatus(m);
+      return autoRenewalSubsStatus(m);
 
     case 'b2b':
       return b2bMemberStatus(m);
