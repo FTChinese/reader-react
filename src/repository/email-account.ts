f2bbd5b99547c842dcb0777d7791d1071679bdf0
weiguo.ni <neefrankie@gmail.com>
2021-11-12 10:51:28 +0800

    feat: http request to modify mobile

--- src/repository/email-account.ts
+++ src/repository/email-account.ts
@@ -6,7 +6,7 @@ import { ResponseError } from './response-error';
 import { Address, AddressFormVal } from '../data/address';
 import { Profile, ProfileFormVal } from '../data/profile';
 import { UpdateEmailReq, UpdateNameFormVal, UpdatePasswordFormVal } from '../data/update-account';
-import { MobileFormVal } from '../data/mobile';
+import { MobileFormVal, VerifySMSFormVal } from '../data/mobile';
 
 export function loadAccount(p: ReaderPassport): Promise<ReaderAccount> {
   return axios.get<undefined, AxiosResponse<ReaderAccount>>(endpoint.emailAccount, {
@@ -32,20 +32,16 @@ export function requestEmailVerification(p: ReaderPassport): Promise<boolean> {
     });
 }
 
-export function updateDisplayName(v: UpdateNameFormVal, p: ReaderPassport): Promise<BaseAccount> {
+export function updateUserName(v: UpdateNameFormVal, token: string): Promise<BaseAccount> {
   return axios.patch<BaseAccount, AxiosResponse<BaseAccount>, UpdateNameFormVal>(
-    endpoint.displayName,
-    v,
-    {
-      headers: authHeader(p)
-    }
-  )
-    .then(resp => {
-      return resp.data;
-    })
-    .catch(error => {
-      return Promise.reject(ResponseError.newInstance(error));
-    });
+      endpoint.displayName,
+      v,
+      {
+        headers: bearerAuthHeader(token)
+      }
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function updateEmail(v: UpdateEmailReq, token: string): Promise<BaseAccount> {
@@ -76,16 +72,32 @@ export function updatePassword(v: UpdatePasswordFormVal, token: string,): Promis
     });
 }
 
+/**
+ * @description Get a verification code
+ * @returns True if success, false if failure.
+ */
 export function requestVerifyMobile(v: MobileFormVal, token: string): Promise<boolean> {
   return axios.put<MobileFormVal, AxiosResponse<boolean>>(
-    endpoint.verifyMobile,
-    v,
-    {
-      headers: bearerAuthHeader(token)
-    }
-  )
+      endpoint.verifyMobile,
+      v,
+      {
+        headers: bearerAuthHeader(token)
+      }
+    )
     .then(resp => resp.status === 204)
     .catch(error => Promise.reject(ResponseError.newInstance(error)));
+}
+
+export function changeMobile(v: VerifySMSFormVal, token: string): Promise<BaseAccount> {
+  return axios.patch<BaseAccount, AxiosResponse<BaseAccount>, VerifySMSFormVal>(
+      endpoint.changeMobile,
+      v,
+      {
+        headers: bearerAuthHeader(token),
+      }
+    )
+    .then(resp => resp.data)
+    .catch(err => Promise.reject(ResponseError.newInstance(err)));
 }
 
 export function loadAddress(token: string): Promise<Address> {
