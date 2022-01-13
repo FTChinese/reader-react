93bbe96510eabbffb43a971c37808356977bff40
weiguo.ni <neefrankie@gmail.com>
2022-01-13 10:27:19 +0800

    feat: stripe prices

--- src/repository/paywall.ts
+++ src/repository/paywall.ts
@@ -1,10 +1,11 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { Paywall } from '../data/paywall';
+import { StripePrice } from '../data/stripe-price';
 import { endpoint } from './endpoint';
 import { ResponseError } from './response-error';
 
 export function loadPaywall(): Promise<Paywall> {
-  return axios.get<unknown, AxiosResponse<Paywall>>(endpoint.paywall,)
+  return axios.get(endpoint.paywall)
     .then(resp => {
       return resp.data;
     })
@@ -13,15 +14,53 @@ export function loadPaywall(): Promise<Paywall> {
     });
 }
 
+export function listStripePrices(): Promise<StripePrice[]> {
+  return axios.get(endpoint.stripePrices)
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
+}
+
+type PriceData = {
+  ftc: Paywall;
+  stripe: Map<string, StripePrice>;
+}
+
 class PaywallRepo {
-  private cached?: Paywall;
+  private cached: Partial<PriceData> = {};
 
   loadPaywall(): Promise<Paywall> {
-    if (this.cached) {
-      return Promise.resolve(this.cached);
+    if (this.cached.ftc) {
+      return Promise.resolve(this.cached.ftc);
     }
 
-    return loadPaywall();
+    return loadPaywall()
+      .then(pw => {
+        this.cached.ftc = pw;
+        return pw
+      });
+  }
+
+  loadStripe(): Promise<Map<string, StripePrice>> {
+    if (this.cached.stripe) {
+      return Promise.resolve(this.cached.stripe);
+    }
+
+    return listStripePrices()
+      .then(prices => {
+        const store = prices.reduce((prev, curr) => {
+          return prev.set(curr.id, curr);
+        }, new Map());
+        this.cached.stripe = store;
+
+        return store;
+      });
+  }
+
+  load(): Promise<[Paywall, Map<string, StripePrice>]> {
+    return Promise.all([
+      this.loadPaywall(),
+      this.loadStripe(),
+    ])
   }
 }
 
