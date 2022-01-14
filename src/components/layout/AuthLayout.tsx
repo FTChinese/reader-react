b415e9cbd1f2fd4b385829314675acb2670b8f09
weiguo.ni <neefrankie@gmail.com>
2022-01-14 16:31:25 +0800

    rename file

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,19 +1,3 @@
-export function CenterLayout(
-  props: {
-    children: JSX.Element
-  }
-) {
-  return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-8 col-lg-6">
-          {props.children}
-        </div>
-      </div>
-    </div>
-  );
-}
-
 export function AuthLayout(
   props: {
     title: string;
