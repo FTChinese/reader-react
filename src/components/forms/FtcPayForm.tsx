a3592e22bf74120d3e5b6879b397992fe16f210e
weiguo.ni <neefrankie@gmail.com>
2022-01-14 16:32:37 +0800

    feat: FtcPayForm

---
+++ src/components/forms/FtcPayForm.tsx
@@ -0,0 +1,65 @@
+import { FormikHelpers, Formik, Form } from 'formik';
+import { useEffect, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import { PaymentMethod } from '../../data/enum';
+import { RadioInput } from '../controls/RadioInput';
+import { SubmitButton } from '../controls/SubmitButton';
+
+export type FtcPayFormVal = {
+  method: PaymentMethod;
+};
+
+export function FtcPayForm(
+  props: {
+    onSubmit: (
+      values: FtcPayFormVal,
+      formikHelpers: FormikHelpers<FtcPayFormVal>
+    ) => void | Promise<any>;
+    errMsg: string;
+  }
+) {
+
+  const [ errMsg, setErrMsg ] = useState('');
+
+  useEffect(() => {
+    setErrMsg(props.errMsg);
+  }, [props.errMsg]);
+
+  return (
+    <>
+      {
+        errMsg &&
+        <Alert
+          variant="danger"
+          dismissible
+          onClose={() => setErrMsg('')}>
+          {errMsg}
+        </Alert>
+      }
+      <Formik<FtcPayFormVal>
+        initialValues={{
+          method: '' as PaymentMethod,
+        }}
+        onSubmit={props.onSubmit}
+      >
+        <Form>
+          <RadioInput
+            id="alipay"
+            name="method"
+            label="支付宝"
+            value="alipay"
+          />
+          <RadioInput
+            id="wechat"
+            name="method"
+            label="微信支付"
+            value="wechat"
+          />
+          <SubmitButton
+            text="支付"
+          />
+        </Form>
+      </Formik>
+    </>
+  );
+}
