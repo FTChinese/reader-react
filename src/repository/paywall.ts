0982f1148b63a92b61d0910e2710caf6c435feb6
weiguo.ni <neefrankie@gmail.com>
2022-01-14 16:44:18 +0800

    feat: create ali and wx order

--- src/repository/paywall.ts
+++ src/repository/paywall.ts
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { bearerAuthHeader } from '../data/account';
+import { OrderParams } from '../data/order';
 import { Paywall } from '../data/paywall';
-import { StripePrice } from '../data/stripe-price';
+import { StripePrice } from '../data/price';
+import { AliPayIntent, WxPayIntent } from '../data/product-shelf';
 import { endpoint } from './endpoint';
 import { ResponseError } from './response-error';
 
@@ -60,8 +63,35 @@ class PaywallRepo {
     return Promise.all([
       this.loadPaywall(),
       this.loadStripe(),
-    ])
+    ]);
   }
 }
 
 export const paywallRepo = new PaywallRepo();
+
+export function createWxOrder(v: OrderParams, token: string): Promise<WxPayIntent> {
+  return axios.post<WxPayIntent, AxiosResponse<WxPayIntent>, OrderParams>(
+      endpoint.aliOrder,
+      v,
+      {
+        headers: bearerAuthHeader(token)
+      }
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
+}
+
+export function createAliOrder(
+  v: OrderParams & { returnUrl: string },
+  token: string
+): Promise<AliPayIntent> {
+  return axios.post<AliPayIntent, AxiosResponse<AliPayIntent>, OrderParams>(
+      endpoint.aliOrder,
+      v,
+      {
+        headers: bearerAuthHeader(token)
+      }
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
+}
