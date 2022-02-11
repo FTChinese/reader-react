94d73bd6b57ee9df932623ed09d789af66311913
weiguo.ni <neefrankie@gmail.com>
2022-02-11 18:20:00 +0800

    refactor

--- src/features/account/OnAccountUpdated.ts
+++ src/features/account/OnAccountUpdated.ts
@@ -1,3 +1,6 @@
 import { BaseAccount } from "../../data/account";
+import { Customer } from '../../data/stripe';
 
 export type OnAccountUpdated = (a: BaseAccount) => void;
+
+export type OnCustomerUpsert = (c: Customer) => void;
