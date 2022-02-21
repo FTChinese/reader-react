9622c0e77fcb2bc467426ed3319da4aebca184a7
weiguo.ni <neefrankie@gmail.com>
2022-02-21 17:23:05 +0800

    feat: ad dd list component

---
+++ src/components/list/DescriptionList.tsx
@@ -0,0 +1,28 @@
+import { StringPair } from './pair';
+
+export function DescriptionList(
+  props: {
+    rows: StringPair[],
+  }
+) {
+  return (
+    <dl>
+      {
+        props.rows.map((row, index) => <DescContent pair={row} key={index} />)
+      }
+    </dl>
+  );
+}
+
+function DescContent(
+  props: {
+    pair: StringPair,
+  }
+) {
+  return (
+    <>
+      <dt>{props.pair[0]}</dt>
+      <dd>{props.pair[1]}</dd>
+    </>
+  )
+}
