7c57dcb7acb3bc7dad639e8fc8323c36accbc9f8
weiguo.ni <neefrankie@gmail.com>
2022-02-21 17:29:57 +0800

    file deleted

--- src/store/useStripePrices.tsx
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-import { StripePrice } from '../data/price';
-
-export const stripePricesState = atom<Map<string, StripePrice>>({
-  key: 'stripePricesState',
-  default: new Map(),
-});
