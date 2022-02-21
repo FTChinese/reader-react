473b882b5abb8897edb84c67ee4b21c809929a97
weiguo.ni <neefrankie@gmail.com>
2022-02-21 17:24:34 +0800

    refactor: spinner

--- src/components/progress/Loading.tsx
+++ src/components/progress/Loading.tsx
@@ -1,13 +1,25 @@
+import Spinner from 'react-bootstrap/Spinner';
+
 export function Loading(
   props: {
     loading: boolean;
     children: JSX.Element;
+    text?: string;
   }
 ) {
   if (props.loading) {
     return (
-      <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
+      <div className="d-flex justify-content-center align-items-center">
+        <Spinner
+          as="span"
+          animation="border"
+        />
+        {
+          props.text &&
+          <span className="ms-2">
+            {props.text}
+          </span>
+        }
       </div>
     );
   }
