7d678a0e01080d4906b4cd134acb771b890d27e9
weiguo.ni <neefrankie@gmail.com>
2022-02-22 11:44:48 +0800

    refactor: move CurrentSubs component

--- src/features/member/CurrentSubs.tsx
+++ src/features/member/CurrentSubs.tsx
@@ -1,34 +1,6 @@
 import { buildMemberStatus } from './member-status'
 import { Membership } from '../../data/membership';
-import { TwoColList } from '../../components/list/TwoColList';
-
-export function CurrentSubs(
-  props: Membership
-) {
-
-  const memberStatus = buildMemberStatus(props);
-
-  return (
-    <div className="card mb-3">
-      <div className="card-header">我的订阅</div>
-      <div className="card-body">
-        <h5 className="card-title text-center">{memberStatus.productName}</h5>
-
-        {
-          memberStatus.reminder &&
-          <p className="text-danger text-center">{memberStatus.reminder}</p>
-        }
-      </div>
-      <TwoColList rows={memberStatus.details}/>
-      {
-        memberStatus.reactivateStripe &&
-        <ReactivateStripe/>
-      }
-    </div>
-  );
-}
-
-function ReactivateStripe() {
+export function ReactivateStripe() {
   return (
     <div className="card-footer text-end">
       <button className="btn btn-primary btn-sm">打开自动续订</button>
--- src/pages/MembershipPage.tsx
+++ src/pages/MembershipPage.tsx
@@ -1,7 +1,10 @@
 import { useAuth } from '../components/hooks/useAuth';
-import { CurrentSubs } from '../features/member/CurrentSubs';
 import { Unauthorized } from '../components/routes/Unauthorized';
 import { SingleCenterCol } from '../components/layout/ContentLayout';
+import { buildMemberStatus } from '../features/member/member-status';
+import { Membership } from '../data/membership';
+import { TwoColList } from '../components/list/TwoColList';
+import { ReactivateStripe } from '../features/member/CurrentSubs';
 
 export function MembershipPage() {
   const { passport } = useAuth();
@@ -16,5 +19,31 @@ export function MembershipPage() {
         {...passport.membership}
       />
     </SingleCenterCol>
+  );
+}
+
+function CurrentSubs(
+  props: Membership
+) {
+
+  const memberStatus = buildMemberStatus(props);
+
+  return (
+    <div className="card mb-3">
+      <div className="card-header">我的订阅</div>
+      <div className="card-body">
+        <h5 className="card-title text-center">{memberStatus.productName}</h5>
+
+        {
+          memberStatus.reminder &&
+          <p className="text-danger text-center">{memberStatus.reminder}</p>
+        }
+      </div>
+      <TwoColList rows={memberStatus.details}/>
+      {
+        memberStatus.reactivateStripe &&
+        <ReactivateStripe/>
+      }
+    </div>
   );
 }
