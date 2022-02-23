594a039e039f9f057611a83d52e2042f7c456606
weiguo.ni <neefrankie@gmail.com>
2022-02-23 12:53:27 +0800

    refactor: remove GoHome

--- src/components/routes/Unauthorized.tsx
+++ src/components/routes/Unauthorized.tsx
@@ -1,14 +1,3 @@
-import { Navigate } from 'react-router-dom';
-import { sitemap } from '../../data/sitemap';
-
 export function Unauthorized() {
   return <div>Logged in required!</div>;
-}
-
-export function GoHome() {
-  return <Navigate
-    to={{
-      pathname: sitemap.home
-    }}
-  />;
 }
