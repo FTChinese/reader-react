8ccee554d1bde73f60bc3cf524b54159ba612226
weiguo.ni <neefrankie@gmail.com>
2022-02-23 15:45:26 +0800

    refactor: StripeSubsDetails

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,14 +1,18 @@
 import Card from 'react-bootstrap/Card';
 import { SingleCenterCol } from '../components/layout/ContentLayout';
 import { Tier } from '../data/enum';
-import { localizeTier } from '../data/localization';
+import { localizeSubsStatus, localizeTier } from '../data/localization';
 import { CartItemUIParams, newFtcCartItemUIParams, newStripeCartItemParams } from '../data/shopping-cart';
 import { AliWxPay } from '../features/checkout/AliWxPay';
 import { PriceCardBody } from '../features/product/PriceCard';
-import { DisplaySubs, StripePay } from '../features/checkout/StripePay';
 import { useShoppingCart } from '../components/hooks/useShoppingCart';
 import { useState } from 'react';
 import { Subs } from '../data/stripe';
+import { Link } from 'react-router-dom';
+import { DescriptionList } from '../components/list/DescriptionList';
+import { StringPair } from '../data/pair';
+import { sitemap } from '../data/sitemap';
+import { StripePay } from '../features/checkout/StripePay';
 
 function ChekcoutLayout(
   props: {
@@ -50,7 +54,7 @@ export function CheckoutPage() {
     return (
       <div className="mt-3">
         <h5 className="text-center">{localizeTier(subs.tier)}订阅成功</h5>
-        <DisplaySubs subs={subs}/>
+        <StripeSubsDetails subs={subs}/>
       </div>
     )
   }
@@ -81,4 +85,30 @@ export function CheckoutPage() {
   } else {
     return <div>Empty shopping cart</div>;
   }
+}
+
+/**
+ * @description Show details of subscription after success.
+ */
+function StripeSubsDetails(
+  props: {
+    subs: Subs,
+  }
+) {
+
+  const rows: StringPair[] = [
+    ['本周期开始时间', `${props.subs.currentPeriodStart}`],
+    ['本周期结束时间', props.subs.currentPeriodEnd],
+    ['订阅状态', localizeSubsStatus(props.subs.status)]
+  ];
+
+  return (
+    <>
+      <DescriptionList rows={rows}/>
+      <p className="scale-down8 text-muted">*周期结束时将自动续订</p>
+      <div className="text-center">
+        <Link to={sitemap.membership}>返回</Link>
+      </div>
+    </>
+  )
 }
