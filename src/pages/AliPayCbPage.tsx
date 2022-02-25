c104239bd36101f3936c9ceebac7cec3eb5cb5be
neefrankie <neefrankie@gmail.com>
2022-02-25 22:23:46 +0800

    feat: verify order and display result

--- src/pages/AliPayCbPage.tsx
+++ src/pages/AliPayCbPage.tsx
@@ -2,19 +2,23 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../components/hooks/useAuth';
 import { useQuery } from '../components/hooks/useQuery';
 import { SingleCenterCol } from '../components/layout/ContentLayout';
+import { HeadTailTable } from '../components/list/HeadTailTable';
 import { ErrorBoundary } from '../components/progress/ErrorBoundary';
 import { Loading } from '../components/progress/Loading';
 import { Unauthorized } from '../components/routes/Unauthorized';
-import { newAliPayCbParams, Order, validateAliPayResp } from '../data/order';
+import { ftcPayResultRows, newAliPayCbParams, PayResult, validateAliPayResp } from '../data/order';
+import { verifyAliWxPay } from '../repository/ftcpay';
+import { ResponseError } from '../repository/response-error';
 import { aliwxPaySession } from '../store/aliwxPaySession';
 
 export function AliPayCbPage() {
 
-  const { passport } = useAuth();
+  const { passport, setMembership } = useAuth();
   const query = useQuery();
 
   const [ progress, setProgress ] = useState(true);
   const [ err, setErr ] = useState('');
+  const [ payResult, setPayResult ] = useState<PayResult>();
 
   if (!passport) {
     return <Unauthorized/>;
@@ -38,16 +42,30 @@ export function AliPayCbPage() {
       return;
     }
 
-
+    verifyAliWxPay(passport.token, o.id)
+      .then(result => {
+        setMembership(result.membership);
+        setProgress(false);
+      })
+      .catch((err: ResponseError) => {
+        setErr(err.toString());
+        setProgress(false);
+      })
   }, []);
 
   return (
     <SingleCenterCol>
       <ErrorBoundary errMsg={err}>
         <Loading loading={progress}>
-          <div className="text-center text-danger">
-            Success!
-          </div>
+          <>
+            {
+              payResult &&
+              <HeadTailTable
+                caption="支付宝支付结果"
+                rows={ftcPayResultRows(payResult)}
+              />
+            }
+          </>
         </Loading>
       </ErrorBoundary>
     </SingleCenterCol>
