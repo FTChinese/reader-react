472ee72cf176c24bf1a7b0d4094244c1c735d52e
weiguo.ni <neefrankie@gmail.com>
2022-03-01 15:19:35 +0800

    refactor: styles

--- src/features/product/ProductCard.tsx
+++ src/features/product/ProductCard.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import { useRecoilValue } from 'recoil';
 import { TextList } from '../../components/list/TextList';
-import { Membership } from '../../data/membership';
+import { isMembershipZero, Membership } from '../../data/membership';
 import { PaywallProduct, productDesc } from '../../data/paywall';
 import {
-  CartItemStripe,
-  buildFtcCartItems,
-  buildStripeCartItems,
+  newCartItemFtc,
+  collectPaywallPrices,
+  collectStripePrices,
+  newCarItemStripe,
 } from '../../data/shopping-cart';
-import { stripePricesState } from '../../components/hooks/stripePriceState';
 import { FtcPriceCard, StripePriceCard } from './PriceCard';
+import { paywallStripeState } from '../../components/hooks/usePaywall';
+import { useRecoilValue } from 'recoil';
 
 export function ProductCard(
   props: {
@@ -18,27 +18,16 @@ export function ProductCard(
   }
 ) {
 
-  const ftcItems = buildFtcCartItems(props.product, props.member);
+  const stripePriceStore = useRecoilValue(paywallStripeState);
 
-  const [stripeItems, setStripeItems] = useState<CartItemStripe[]>();
+  const { ftcPrices, stripeIds } = collectPaywallPrices(props.product, isMembershipZero(props.member));
 
-  const stripePrices = useRecoilValue(stripePricesState);
+  console.log(ftcPrices);
 
-  useEffect(() => {
-    if (stripePrices.size === 0) {
-      return;
-    }
+  const stripePrices = collectStripePrices(stripeIds, stripePriceStore);
 
-    setStripeItems(buildStripeCartItems({
-      product: props.product,
-      m: props.member,
-      prices: stripePrices,
-    }));
-
-  }, [stripePrices.size]);
-
-  function colClass(count: number): string {
-    let className = 'mt-3 row row-cols-1';
+  function rowClass(count: number): string {
+    let className = 'row row-cols-1';
     switch (count) {
       case 1:
         return className;
@@ -57,26 +46,27 @@ export function ProductCard(
         {props.product.heading}
       </h5>
 
-      <div className={colClass(ftcItems.length)}>
-        {ftcItems.map((item, i) => (
-          <div className="col" key={i}>
-            <FtcPriceCard item={item} />
-          </div>
-        ))}
+      <div className={rowClass(ftcPrices.length)}>
+        {
+          ftcPrices.map((price, i) => (
+            <div className="col mb-3" key={i}>
+              <FtcPriceCard
+                item={newCartItemFtc(price, props.member)}
+              />
+            </div>
+          ))
+        }
       </div>
 
-      {
-        stripeItems &&
-        <div className={colClass(stripeItems.length)}>
-          {
-            stripeItems.map((item, i) => (
-              <div className="col mb-3" key={i}>
-                <StripePriceCard item={item} />
-              </div>
-            ))
-          }
-        </div>
-      }
+      <div className={rowClass(stripePrices.length)}>
+        {
+          stripePrices.map((price, i) => (
+            <div className="col mb-3" key={i}>
+              <StripePriceCard item={newCarItemStripe(price, props.member)} />
+            </div>
+          ))
+        }
+      </div>
 
       <small className="text-muted">
         * 自动续订通过<a href="https://stripe.com/" target="_blank">Stripe</a>支付，以英镑计价，需使用支持国际货币的信用卡
