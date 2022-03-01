283ffc666da95acfc88c714339d1286c8c69144d
weiguo.ni <neefrankie@gmail.com>
2022-03-01 15:20:35 +0800

    refactor

--- src/pages/SubsPage.tsx
+++ src/pages/SubsPage.tsx
@@ -1,56 +1,108 @@
 import { useState, useEffect } from 'react';
-import { useRecoilState } from 'recoil';
-import { stripePricesState } from '../components/hooks/stripePriceState';
+import { useRecoilValue } from 'recoil';
+import { ImageRatio } from '../components/graphics/ImageRatio';
 import { useAuth } from '../components/hooks/useAuth';
+import { paywallBannerState, paywallProductsState, usePaywall } from '../components/hooks/usePaywall';
 import { ErrorBoundary } from '../components/progress/ErrorBoundary';
 import { Loading } from '../components/progress/Loading';
 import { Unauthorized } from '../components/routes/Unauthorized';
-import { Paywall } from '../data/paywall';
-import { PaywallContent } from '../features/product/PaywallContent';
-import { paywallRepo } from '../repository/paywall';
+import { ProductCard } from '../features/product/ProductCard';
+import { listStripePrices, loadPaywall } from '../repository/paywall';
 import { ResponseError } from '../repository/response-error';
 
 export function SubsPage() {
 
-  const { passport } = useAuth();
-
-  if (!passport) {
-    return <Unauthorized/>;
-  }
-
-  const [ _, setPrices ] = useRecoilState(stripePricesState);
-
-  const [ paywall, setPaywall ] = useState<Paywall>();
-  const [ progress, setProgress ]= useState(false);
+  const [ progress, setProgress ]= useState(true);
   const [ err, setErr ] = useState('');
 
+  const { setFtcPrice, setStripePrice } = usePaywall();
+
   useEffect(() => {
-    paywallRepo.load()
-      .then(([ftc, stripe]) => {
+    loadPaywall()
+      .then(pw => {
+        console.log(pw)
+        setFtcPrice(pw);
         setProgress(false);
-        setPaywall(ftc);
-        setPrices(stripe);
       })
       .catch((err: ResponseError) => {
-        setProgress(false);
+        console.log(err);
         setErr(err.message);
+        setProgress(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    listStripePrices()
+      .then(prices => {
+        setStripePrice(prices);
+        console.log(prices);
+      })
+      .catch((err: ResponseError) => {
+        console.log(err);
       });
   }, []);
 
   return (
     <ErrorBoundary errMsg={err}>
       <Loading loading={progress}>
-        <>
-          {
-            paywall &&
-            <PaywallContent
-              passport={passport}
-              paywall={paywall}
-            />
-          }
-        </>
-
+        <div className="mt-3">
+          <PaywallBanner/>
+          <PaywallProducts/>
+        </div>
       </Loading>
     </ErrorBoundary>
+  );
+}
+
+function PaywallBanner() {
+
+  const banner = useRecoilValue(paywallBannerState);
+
+  if (!banner) {
+    return null;
+  }
+
+  return (
+    <div className="row flex-row-reverse">
+      <div className="col-sm-4">
+        <ImageRatio
+          src={banner.coverUrl}
+        />
+      </div>
+
+      <div className="col-sm-8">
+        <h2>{banner.heading}</h2>
+        <h3>{banner.subHeading}</h3>
+        <div>{banner.content}</div>
+      </div>
+    </div>
+  );
+}
+
+function PaywallProducts() {
+  const { passport } = useAuth();
+
+  const products = useRecoilValue(paywallProductsState);
+
+  if (!passport) {
+    return <Unauthorized/>;
+  }
+
+  if (products.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="mt-3">
+      {
+        products.map(product => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            member={passport.membership}
+          />
+        ))
+      }
+    </div>
   );
 }
