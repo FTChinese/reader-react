b61e786c558d96a8a1cb6f799e6f80693dc78884
weiguo.ni <neefrankie@gmail.com>
2022-03-02 11:39:03 +0800

    feat: cancel subscription

--- src/features/checkout/StripeSettings.tsx
+++ src/features/checkout/StripeSettings.tsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useAuth } from '../../components/hooks/useAuth';
 import { ChevronDown, ChevronUp } from '../../components/graphics/icons';
-import { loadSubsDefaultPayMethod } from '../../repository/stripe';
+import { cancelSubs, loadSubsDefaultPayMethod } from '../../repository/stripe';
 import { PaymentMethodSelector } from './PaymentMethodSelector';
 import { StripeDefaultPaymentMethod } from './StripDefaultPaymentMethod';
 import { BorderHeader } from '../../components/header/BorderHeader';
-import { RowContainer } from '../../components/list/RowCotainer';
+import { RowContainer, RowSecondary, RowTrailButton } from '../../components/list/RowCotainer';
+import { TwoColHeader } from '../../components/header/TwoColHeader';
+import Modal from 'react-bootstrap/Modal';
+import { ProgressButton } from '../../components/buttons/ProgressButton';
+import Alert from 'react-bootstrap/Alert';
+import { ResponseError } from '../../repository/response-error';
 
 export function StripeSettings() {
   return (
@@ -16,9 +21,8 @@ export function StripeSettings() {
         level={5}
       />
 
-      <RowContainer>
-        <PaymentMethodSetting/>
-      </RowContainer>
+      <PaymentMethodSetting/>
+      <StripeCancelSubs/>
     </div>
   );
 }
@@ -44,33 +48,125 @@ function PaymentMethodSetting() {
   );
 
   return (
-    <>
-      <PaymentMethodSelector/>
+    <RowContainer>
+      <>
+        <PaymentMethodSelector/>
 
-      <div className="d-flex align-items-center">
-        <span className="me-1 scale-down8 text-black50">添加或更改当前订阅的支付方式</span>
-        <Button
-          variant="link"
-          onClick={() => setShow(!show)}
-        >
-          <span className="scale-down8">详情</span>
-          {
-            show ?
-            <ChevronUp/> :
-            <ChevronDown/>
-          }
+        <div className="d-flex align-items-center scale-down8 text-black50">
+          <span className="me-1">添加或更改当前订阅的支付方式</span>
+          <Button
+            variant="link"
+            onClick={() => setShow(!show)}
+          >
+            <span className="scale-down8">详情</span>
+            {
+              show ?
+              <ChevronUp/> :
+              <ChevronDown/>
+            }
 
-        </Button>
-      </div>
+          </Button>
+        </div>
 
-      {
-        show &&
-        <StripeDefaultPaymentMethod
-          passport={passport}
-          load={load}
-        />
-      }
-    </>
+        {
+          show &&
+          <StripeDefaultPaymentMethod
+            passport={passport}
+            load={load}
+          />
+        }
+      </>
+    </RowContainer>
   );
 }
 
+function StripeCancelSubs() {
+
+  const [ show, setShow ] = useState(false);
+
+  return (
+    <RowContainer>
+      <>
+        <TwoColHeader
+          left={<h6>关闭自动续订</h6>}
+          right={
+            <RowTrailButton
+              text="关闭"
+              onClick={() => setShow(true)}
+            />
+          }
+        />
+        <RowSecondary>
+          <span>关闭自动续订将在本次订阅到期后停止扣款</span>
+        </RowSecondary>
+
+        <CancelSubsDialog
+          show={show}
+          onHide={() => setShow(false)}
+        />
+      </>
+    </RowContainer>
+  );
+}
+
+function CancelSubsDialog(
+  props: {
+    show: boolean;
+    onHide: () => void;
+  }
+) {
+
+  const { passport, setMembership } = useAuth();
+  const [ progress, setProgress ] = useState(false);
+  const [ err, setErr ] = useState('');
+
+  const handleClick = () => {
+    if (!passport) {
+      return;
+    }
+
+    const subsId = passport.membership.stripeSubsId;
+    if (!subsId) {
+      return;
+    }
+
+    cancelSubs(passport.token, subsId)
+      .then( result => {
+        setMembership(result.membership);
+        setProgress(false);
+      })
+      .catch((err: ResponseError) => {
+        setErr(err.message);
+        setProgress(false);
+      });
+  };
+
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>
+          关闭自动续订？
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        {
+          err && <Alert>{err}</Alert>
+        }
+        <p>当前订阅周期到期后将不再从您的默认支付方式中扣款。</p>
+        <p>当前订阅周期结束前您可以随时恢复自动续订。</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <ProgressButton
+          progress={progress}
+          disabled={progress}
+          text="是的，我要关闭"
+          onClick={handleClick}
+        />
+      </Modal.Footer>
+    </Modal>
+  );
+}
