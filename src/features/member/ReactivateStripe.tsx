c45db1afd00c294ae89f08f8d849d3a9b505ff84
weiguo.ni <neefrankie@gmail.com>
2022-03-02 16:12:22 +0800

    removed file

--- src/features/member/ReactivateStripe.tsx
+++
@@ -1,9 +0,0 @@
-import { buildMemberStatus } from './member-status'
-import { Membership } from '../../data/membership';
-export function ReactivateStripe() {
-  return (
-    <div className="card-footer text-end">
-      <button className="btn btn-primary btn-sm">打开自动续订</button>
-    </div>
-  );
-}
