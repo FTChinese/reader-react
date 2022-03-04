e821bfae30b750840aef83bb2fa23f4ced998f16
weiguo.ni <neefrankie@gmail.com>
2022-03-04 16:25:59 +0800

    feat: RequireStripe

---
+++ src/components/routes/RequireStripe.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { stripePromise } from '../../features/checkout/StripeContext';
+import { Loading } from '../progress/Loading';
+
+export function RequireStripe(
+  props: {
+    children: JSX.Element
+  }
+) {
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    stripePromise.then(s => {
+      if (!s) {
+        toast.error('Stripe initialization failed!')
+        return;
+      }
+
+      setLoading(false);
+    });
+
+  }, []);
+
+  return (
+    <Loading loading={loading}>
+      {props.children}
+    </Loading>
+  );
+}
