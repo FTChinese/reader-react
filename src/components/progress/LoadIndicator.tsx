d2f9c21ce93b09b7b68267ed716ee523f6436916
weiguo.ni <neefrankie@gmail.com>
2022-03-04 16:31:41 +0800

    refactor: removed CircleLoader

--- src/components/progress/LoadIndicator.tsx
+++ src/components/progress/LoadIndicator.tsx
@@ -1,29 +1,6 @@
 import { Spinner } from 'react-bootstrap';
 import { Variant } from 'react-bootstrap/types';
 
-/**
- * @deprecated
- */
-export function LoadIndicator(
-  props: {
-    progress: boolean;
-    small?: boolean;
-    variant?: Variant
-  }
-) {
-
-  if (props.progress) {
-    return <Spinner
-      as="span"
-      animation="border"
-      size={props.small ? 'sm' : undefined}
-      variant={props.variant}
-    />;
-  }
-
-  return null;
-}
-
 export function CircleLoader(
   props: {
     progress: boolean;
