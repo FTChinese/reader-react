ea7976d61f85bc591b7e3e3810f52f4217b273fe
weiguo.ni <neefrankie@gmail.com>
2022-03-07 17:23:34 +0800

    feat: optional bottom border

--- src/components/layout/Flex.tsx
+++ src/components/layout/Flex.tsx
@@ -3,13 +3,14 @@ export function Flex(
     children: JSX.Element;
     justify?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly';
     align?: 'start' | 'end' | 'center' | 'baseline' | 'stretch';
+    border?: boolean;
   }
 ) {
 
   const justify = props.justify || 'between';
   const align = props.align || 'start';
 
-  const className = `d-flex justify-content-${justify} align-items-${align}`;
+  const className = `d-flex justify-content-${justify} align-items-${align}${props.border ? ' border-bottom' : ''}`;
 
   return (
     <div className={className}>
