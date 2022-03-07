a4c1bec374754f4018750a0ad3ba71f2abdd07b8
weiguo.ni <neefrankie@gmail.com>
2022-03-07 14:31:55 +0800

    refactor: require stripe middleware

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -6,6 +6,8 @@ import { AliWxPay } from '../features/checkout/AliWxPay';
 import { PriceCard } from '../features/product/PriceCard';
 import { useShoppingCart } from '../components/hooks/useShoppingCart';
 import { StripePay } from '../features/checkout/StripePay';
+import { useAuth } from '../components/hooks/useAuth';
+import { RequireStripeCustomer } from '../components/routes/RequireStripeCustomer';
 
 function ChekcoutLayout(
   props: {
@@ -38,7 +40,12 @@ function ChekcoutLayout(
  * - After a new stripe payment method is added and user is redirected here.
  */
 export function CheckoutPage() {
+  const { passport } = useAuth();
   const { cart } = useShoppingCart();
+
+  if (!passport) {
+    return null;
+  }
 
   if (cart.ftc) {
     return (
@@ -53,14 +60,16 @@ export function CheckoutPage() {
     )
   } else if (cart.stripe) {
     return (
-      <ChekcoutLayout
-        tier={cart.stripe.recurring.tier}
-        params={newStripeCartItemParams(cart.stripe)}
-      >
-        <StripePay
-          item={cart.stripe}
-        />
-      </ChekcoutLayout>
+      <RequireStripeCustomer>
+        <ChekcoutLayout
+          tier={cart.stripe.recurring.tier}
+          params={newStripeCartItemParams(cart.stripe)}
+        >
+          <StripePay
+            item={cart.stripe}
+          />
+        </ChekcoutLayout>
+      </RequireStripeCustomer>
     );
   } else {
     return <div>Empty shopping cart</div>;
