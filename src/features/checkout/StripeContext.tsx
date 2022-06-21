1a3895920a9ba4913e4b92b60c5595cc1fb26e91
weiguo.ni <neefrankie@gmail.com>
2022-06-21 16:35:30 +0800

    refactor: doc

--- src/features/checkout/StripeContext.tsx
+++ src/features/checkout/StripeContext.tsx
@@ -2,6 +2,12 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js';
 import { loadStripePubKey } from '../../repository/stripe';
 
+// See https://stripe.com/docs/stripe-js/react
+// The requirment of Stripe.js initialization makes
+// it impossible to use both test and live key
+// simutaneously.
+// A possible solution is is to set up two
+// servers using different keys.
 export const stripePromise = loadStripePubKey()
 .then(pk => {
   console.log('Initializing Stripe...');
