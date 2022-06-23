98316b373a42dd0025059aa239b47f16d37ff0e8
weiguo.ni <neefrankie@gmail.com>
2022-06-23 11:47:01 +0800

    cleanup

--- src/data/paywall.ts
+++ src/data/paywall.ts
@@ -1,45 +1,7 @@
-import { OfferKind, Tier } from './enum';
-import { Discount, filterOffers, Price } from './price';
-import { formatMoney } from './localization';
-import { cycleOfYMD, isValidPeriod, OptionalPeriod, totalDaysOfYMD } from './period';
-
-type DailyPrice = {
-  holder: string;
-  replacer: string;
-}
-
-export function dailyPrice(price: Price): DailyPrice {
-  const days = totalDaysOfYMD(price.periodCount) || 1;
-
-  const avg = price.unitAmount / days;
-
-  const dailyAmount = formatMoney(price.currency, avg);
-
-  switch (cycleOfYMD(price.periodCount)) {
-    case 'year':
-      return {
-        holder: '{{dailyAverageOfYear}}',
-        replacer: dailyAmount,
-      };
-
-    case 'month':
-      return {
-        holder: '{{dailyAverageOfMonth}}',
-        replacer: dailyAmount,
-      };
-  }
-}
-
-export type Product = {
-  id: string;
-  active: boolean;
-  description: string;
-  heading: string;
-  introductory?: Price;
-  liveMode: boolean;
-  smallPrint?: string;
-  tier: Tier;
-};
+import { isValidPeriod, OptionalPeriod } from './period';
+import { buildProductItem, PaywallProduct, ProductItem } from './paywall-product';
+import { StripePaywallItem } from './stripe';
+import { Membership } from './membership';
 
 export type Banner = {
   id: string;
@@ -59,60 +21,30 @@ export function isPromoValid(promo: Promo): Boolean {
 
   return isValidPeriod(promo);
 }
-
-/**
- * @description PaywallPrice contains a price and a list of
- * opitonal discounts.
- */
- export type PaywallPrice = Price & {
-  offers: Discount[];
-};
-
-export function applicableOffer(pp: PaywallPrice, filters: OfferKind[]): Discount | undefined {
-  if (pp.offers.length === 0) {
-    return undefined;
-  }
-
-  const filtered = filterOffers(pp.offers, filters);
-
-  if (filtered.length == 0) {
-    return undefined;
-  }
-
-  return filtered[0];
-}
-
-/**
- * @description PaywallProduct contains the human-readable text of a product, and a list of prices attached to it.
- */
-export type PaywallProduct = Product & {
-  prices: PaywallPrice[];
-};
-
-/**
- * @description Turn a product's description into
- * an array of strings.
- */
-export function productDesc(product: PaywallProduct): string[] {
-  if (!product.description) {
-    return [];
-  }
-
-  let desc = product.description;
-
-  product.prices
-    .map(price => dailyPrice(price))
-    .forEach(dp => desc = desc.replace(dp.holder, dp.replacer));
-
-  return desc.split('\n');
-}
-
 export interface Paywall {
   id: number;
   banner: Banner;
   liveMode: boolean;
   promo: Promo;
   products: PaywallProduct[];
+  stripe: StripePaywallItem[];
+}
+
+export function buildProductItems(paywall: Paywall, m: Membership): ProductItem[] {
+  // Index StripePaywallitem by price id.
+  const stripeItemStore = paywall.stripe.reduce((store, curr) => {
+    return store.set(curr.price.id, curr);
+  }, new Map<string, StripePaywallItem>());
+
+  return paywall.products.map(product => {
+    return buildProductItem(
+      product,
+      {
+        membership: m,
+        stripeStore: stripeItemStore,
+      }
+    )
+  });
 }
 
 
