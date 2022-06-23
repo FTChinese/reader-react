1443fa0b81b03bffb63cc07cd97249c5ff5daf3b
weiguo.ni <neefrankie@gmail.com>
2022-06-23 11:48:25 +0800

    refactor: change component to stateless

--- src/features/product/ProductCard.tsx
+++ src/features/product/ProductCard.tsx
@@ -1,31 +1,21 @@
 import { TextList } from '../../components/list/TextList';
-import { isMembershipZero, Membership } from '../../data/membership';
-import { PaywallProduct, productDesc } from '../../data/paywall';
 import {
-  newCartItemFtc,
-  collectPaywallPrices,
-  collectStripePrices,
-  newCarItemStripe,
+  CartItemFtc,
+  cartItemOfStripe,
+  CartItemStripe,
+  cartItemUiOfFtc,
 } from '../../data/shopping-cart';
-import { FtcPriceCard, StripePriceCard } from './PriceCard';
-import { paywallStripeState } from '../../components/hooks/usePaywall';
-import { useRecoilValue } from 'recoil';
+import { PriceCard } from './PriceCard';
 import { StripePayLink } from './StripePayLink';
+import { ProductItem } from '../../data/paywall-product';
 
 export function ProductCard(
   props: {
-    product: PaywallProduct;
-    member: Membership;
+    item: ProductItem;
+    onFtcPay: (item: CartItemFtc) => void;
+    onStripePay: (item: CartItemStripe) => void;
   }
 ) {
-
-  const stripePriceStore = useRecoilValue(paywallStripeState);
-
-  const { ftcPrices, stripeIds } = collectPaywallPrices(props.product, isMembershipZero(props.member));
-
-  console.log(ftcPrices);
-
-  const stripePrices = collectStripePrices(stripeIds, stripePriceStore);
 
   function rowClass(count: number): string {
     let className = 'row row-cols-1';
@@ -44,26 +34,36 @@ export function ProductCard(
   return (
     <div className="h-100 p-3">
       <h5 className="text-center mb-3 pb-3 border-bottom">
-        {props.product.heading}
+        {props.item.content.heading}
       </h5>
 
-      <div className={rowClass(ftcPrices.length)}>
+      <div className={rowClass(props.item.ftcItems.length)}>
         {
-          ftcPrices.map((price, i) => (
-            <div className="col mb-3" key={i}>
-              <FtcPriceCard
-                item={newCartItemFtc(price, props.member)}
+          props.item.ftcItems.map(cartItem => (
+            <div
+              className="col mb-3"
+              key={cartItem.price.id}
+            >
+              <PriceCard
+                params={ cartItemUiOfFtc(cartItem) }
+                onClick={ () => props.onFtcPay(cartItem) }
               />
             </div>
           ))
         }
       </div>
 
-      <div className={rowClass(stripePrices.length)}>
+      <div className={rowClass(props.item.stripeItems.length)}>
         {
-          stripePrices.map((price, i) => (
-            <div className="col mb-3" key={i}>
-              <StripePriceCard item={newCarItemStripe(price, props.member)} />
+          props.item.stripeItems.map(cartItem => (
+            <div
+              className="col mb-3"
+              key={cartItem.recurring.id}
+            >
+              <PriceCard
+                params={cartItemOfStripe(cartItem)}
+                onClick={ () => props.onStripePay(cartItem) }
+              />
             </div>
           ))
         }
@@ -72,11 +72,11 @@ export function ProductCard(
       <StripePayLink />
 
       <TextList
-        lines={productDesc(props.product)}
+        lines={props.item.content.description}
         className="mt-3" />
 
       <small className="text-muted">
-        {props.product.smallPrint}
+        {props.item.content.smallPrint}
       </small>
     </div>
   );
