cb356c9bade2dff56f89ffe6a052b47ba48d7302
weiguo.ni <neefrankie@gmail.com>
2022-06-29 10:53:23 +0800

    refactor: fetch payment methods upon dialog open

--- src/features/stripepay/PaymentMethodDialog.tsx
+++ src/features/stripepay/PaymentMethodDialog.tsx
@@ -1,19 +1,41 @@
 import Modal from 'react-bootstrap/Modal';
 import { FullscreenSingleCol } from '../../components/layout/FullscreenSingleCol';
 import { SetupPaymentMethod } from './SetupPaymentMethod';
-import { CustomerPaymentMethods } from './CustomerPaymentMethods';
 import { SetupUsage } from '../../store/stripeSetupSession';
 import { ReaderPassport } from '../../data/account';
-import { PaymentMethod } from '../../data/stripe';
+import { StripePayMethod } from '../../data/stripe';
+import { useStripePaySetting } from '../../components/hooks/useStripePaySetting';
+import { ErrorBoundary } from '../../components/progress/ErrorBoundary';
+import { Loading } from '../../components/progress/Loading';
+import { BankCardRow } from './BankCardRow';
+import { useEffect } from 'react';
 
 export function PaymentMethodDialog(
   props: {
     show: boolean;
     passport: ReaderPassport;
     onHide: () => void;
-    onMethodSelected: (method: PaymentMethod) => void;
   }
 ) {
+
+  const {
+    defaultPayMethod,
+    selectedPayMethod,
+    setSelectedPayMethod,
+    loading,
+    loadErr,
+    paymentMethods,
+    listPaymentMethods,
+  } = useStripePaySetting();
+
+  useEffect(() => {
+    const cusId = props.passport.stripeId;
+    if (!cusId) {
+      return;
+    }
+
+    listPaymentMethods(props.passport.token, cusId);
+  }, []);
 
   return (
     <Modal
@@ -28,10 +50,17 @@ export function PaymentMethodDialog(
       <Modal.Body>
         <FullscreenSingleCol>
           <>
-            <CustomerPaymentMethods
-              passport={props.passport}
-              selectable={true}
-            />
+            <ErrorBoundary errMsg={loadErr}>
+              <Loading loading={loading}>
+                <ListPaymentMethods
+                  methods={paymentMethods}
+                  defaultMethod={defaultPayMethod}
+                  selectedMethod={selectedPayMethod}
+                  onSelect={setSelectedPayMethod}
+                />
+              </Loading>
+            </ErrorBoundary>
+
             <SetupPaymentMethod
               passport={props.passport}
               usage={SetupUsage.PayNow}
@@ -43,7 +72,37 @@ export function PaymentMethodDialog(
   );
 }
 
+function ListPaymentMethods(
+  props: {
+    methods: StripePayMethod[];
+    defaultMethod?: StripePayMethod;
+    selectedMethod?: StripePayMethod;
+    onSelect: (pm: StripePayMethod) => void;
+  }
+) {
 
+  if (props.methods.length == 0) {
+    return <div>尚未设置任何支付方式</div>;
+  }
+
+  return (
+    <>
+      {
+        props.methods.map(m => (
+          <BankCardRow
+            key={m.id}
+            paymentMethod={m}
+            selected={m.id === props.selectedMethod?.id}
+            isDefault={m.id === props.defaultMethod?.id}
+            onSelect={() => {
+              props.onSelect(m);
+            }}
+          />
+        ))
+      }
+    </>
+  );
+}
 
 
 
