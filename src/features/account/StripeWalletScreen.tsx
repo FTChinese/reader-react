4cb7a09f674828667ebc604c5ee83fea05d157cc
weiguo.ni <neefrankie@gmail.com>
2022-06-29 10:52:01 +0800

    feat: StripeWalletScreen

---
+++ src/features/account/StripeWalletScreen.tsx
@@ -0,0 +1,79 @@
+import { BankCard } from '../../components/BankCard';
+import { SpinnerOrText, TextButton } from '../../components/buttons/Buttons';
+import { Flex } from '../../components/layout/Flex';
+import { PaymentMethodSelector } from '../../components/text/Checkout';
+import { StripePayMethod } from '../../data/stripe';
+
+/**
+ * onSetDefault callback is used to set default payment method. * A default payment method could be set
+ * either on a customer.invoice_settings.default_payment_method
+ * or subscription.default_payment_method.
+ * We employ this approach:
+ * - If user has an active subscription, set the payment method on subscription, which actually means you are modifying this subscritpion;
+ * - If user does not have an active subscription, set it on customer's invoice settings.
+ * @see https://stripe.com/docs/api/customers/object
+ * @see https://stripe.com/docs/api/subscriptions/object
+ */
+export function StripeWalletScreen(
+  props: {
+    paymentMethod?: StripePayMethod;
+    isDefault: boolean;
+    submitting: boolean;
+    onSetDefault: (method: StripePayMethod) => void;
+    onAddCard: () => void;
+  }
+) {
+
+  const card = props.paymentMethod?.card;
+
+  return (
+    <>
+
+      <PaymentMethodSelector
+        onClick={props.onAddCard}
+      />
+
+      {
+        card &&
+        <BankCard
+          brand={card.brand}
+          last4={card.last4}
+          expYear={card.expYear}
+          expMonth={card.expMonth}
+        />
+      }
+
+      <SetAsDefault
+        submitting={props.submitting}
+        isDefault={props.isDefault}
+        onClick={() => {
+          if (props.paymentMethod) {
+            props.onSetDefault(props.paymentMethod);
+          }
+        }}
+      />
+    </>
+  );
+}
+
+function SetAsDefault(
+  props: {
+    submitting: boolean;
+    isDefault: boolean;
+    onClick: () => void;
+  }
+) {
+  return (
+    <Flex justify="end">
+      <TextButton
+        onClick={props.onClick}
+        disabled={props.isDefault || props.submitting}
+      >
+        <SpinnerOrText
+          text="设为默认"
+          progress={props.submitting}
+        />
+      </TextButton>
+    </Flex>
+  );
+}
