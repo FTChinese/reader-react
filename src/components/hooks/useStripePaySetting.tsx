7a8416c431424a42aca293dbd320c935b08f4fda
weiguo.ni <neefrankie@gmail.com>
2022-06-29 10:49:52 +0800

    refactor: add methods to modify stripe paymemt methods

--- src/components/hooks/useStripePaySetting.tsx
+++ src/components/hooks/useStripePaySetting.tsx
@@ -1,61 +1,92 @@
+import { useState } from 'react';
 import { atom, useRecoilState } from 'recoil';
-import { PaymentMethod } from '../../data/stripe';
+import { ReaderPassport } from '../../data/account';
+import { StripePayMethod } from '../../data/stripe';
+import { ResponseError } from '../../repository/response-error';
+import { stripeRepo } from '../../repository/stripe';
 
-/**
- * @description This is the single source of truth
- * for user's selected payment method when using Stripe.
- * It might comes from multiple sources:
- * - Retrieve from customer's default payment method
- * - User selected from a list of available payment methods
- * - A new payment method added via PaymentMethodDialog and populated from StripeSetupCbpage after successful redirection.
- */
-type StripePaymentSetting = {
-  defaultMethod?: PaymentMethod;
-  // The selected payment method might comes from
-  // multiple sources:
-  // * If user already selected one from list, use it;
-  // * otherwise load user's default payment method.
-  selectedMethod?: PaymentMethod;
-  methodCandidates: PaymentMethod[];
-}
+const defaultPayMethodState = atom<StripePayMethod | undefined>({
+  key: 'stripeDefaultPayMethod',
+  default: undefined,
+});
 
-const paymentSettingState = atom<StripePaymentSetting>({
-  key: 'customerPaymentMethods',
-  default: {
-    defaultMethod: undefined,
-    selectedMethod: undefined,
-    methodCandidates: [],
-  },
+const selectedPayMethodState = atom<StripePayMethod | undefined>({
+  key: 'stripeSelectedPayMethod',
+  default: undefined,
+});
+
+const payMethodListState = atom<StripePayMethod[]>({
+  key: 'stripePayMethodList',
+  default: [],
 });
 
 export function useStripePaySetting() {
-  const [ paymentSetting, setPaymentSetting ] = useRecoilState(paymentSettingState);
+  const [ defaultPayMethod, setDefaultPayMethod ] = useRecoilState(defaultPayMethodState);
+  const [ selectedPayMethod, setSelectedPayMethod ] = useRecoilState(selectedPayMethodState);
 
-  function selectPaymentMethod(pm: PaymentMethod) {
-    setPaymentSetting({
-      ...paymentSetting,
-      selectedMethod: pm,
-    });
-  }
-
-  function setDefaultPaymentMethod(pm: PaymentMethod) {
-    setPaymentSetting({
-      ...paymentSetting,
-      defaultMethod: pm,
-    });
-  }
+  const [ loading, setLoading ] = useState(false);
+  const [ loadErr, setLoadErr ] = useState('');
+  const [ paymentMethods, setPaymentMethods ] = useRecoilState(payMethodListState);
 
   function clearPaymentSetting() {
-    setPaymentSetting({
-      selectedMethod: undefined,
-      methodCandidates: [],
-    });
+    setDefaultPayMethod(undefined);
+    setSelectedPayMethod(undefined);
   }
 
+  const onPayMethodUpdated = (method: StripePayMethod) => {
+    setSelectedPayMethod(method);
+    setDefaultPayMethod(method);
+  };
+
+  const loadDefaultPaymentMethod = (passport: ReaderPassport) => {
+    setLoadErr('');
+    setLoading(true);
+
+    stripeRepo.loadDefaultPayment(passport)
+      .then(pm => {
+        setLoading(false);
+        onPayMethodUpdated(pm);
+      })
+      .catch((err: ResponseError) => {
+        setLoading(false);
+        setLoadErr(err.message);
+      });
+  };
+
+  const listPaymentMethods = (token: string, cusId: string) => {
+    if (paymentMethods.length > 0) {
+      return;
+    }
+
+    setLoadErr('');
+    setLoading(true);
+
+    stripeRepo.listPaymentMethods(
+      token,
+      cusId
+    )
+    .then(list => {
+      setLoading(false);
+      setPaymentMethods(list.data);
+    })
+    .catch((err: ResponseError) => {
+      setLoading(false);
+      setLoadErr(err.toString());
+    });
+  };
+
   return {
-    paymentSetting,
-    setDefaultPaymentMethod,
-    selectPaymentMethod,
+    defaultPayMethod,
+    selectedPayMethod,
+    setSelectedPayMethod,
+    onPayMethodUpdated,
+
+    loadDefaultPaymentMethod,
+    loading,
+    loadErr,
+    paymentMethods,
+    listPaymentMethods,
+
     clearPaymentSetting,
   };
 }
