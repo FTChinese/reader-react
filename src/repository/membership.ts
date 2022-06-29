e6acf55ac00d8ba9edb29abedbabe1e9021bbb76
weiguo.ni <neefrankie@gmail.com>
2022-06-29 16:52:59 +0800

    refactor: replace axios

--- src/repository/membership.ts
+++ src/repository/membership.ts
@@ -1,27 +1,17 @@
-import axios from 'axios';
-import { bearerAuthHeader } from '../data/account';
 import { Membership } from '../data/membership';
 import { endpoint } from './endpoint';
-import { ResponseError } from './response-error';
+import { Fetch } from './request';
 
 export function reloadMembership(token: string): Promise<Membership> {
-  return axios.get<Membership>(
-      endpoint.membership,
-      {
-        headers: bearerAuthHeader(token)
-      }
-    )
-    .then(resp => resp.data)
-    .catch(error => Promise.reject(ResponseError.fromAxios(error)));
+  return new Fetch()
+    .setBearerAuth(token)
+    .get(endpoint.membership)
+    .endJson<Membership>();
 }
 
 export function claimAddOn(token: string): Promise<Membership> {
-  return axios.post<Membership>(
-      endpoint.memberAddon,
-      {
-        Headers: bearerAuthHeader(token)
-      }
-    )
-    .then(resp => resp.data)
-    .catch(error => Promise.reject(ResponseError.fromAxios(error)));
+  return new Fetch()
+    .setBearerAuth(token)
+    .post(endpoint.memberAddon)
+    .endJson<Membership>();
 }
