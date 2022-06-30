8ad4a5379ee52fdafa98beabecf8ec9d7b88c821
weiguo.ni <neefrankie@gmail.com>
2022-06-30 15:12:19 +0800

    refactor: stripe setup callback

--- src/pages/StripeSetupCbPage.tsx
+++ src/pages/StripeSetupCbPage.tsx
@@ -3,27 +3,23 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { ErrorBoundary } from '../components/progress/ErrorBoundary';
 import { Loading } from '../components/progress/Loading';
 import { useAuth } from '../components/hooks/useAuth';
-import { newSetupCbParams, SetupUsage, stripeSetupSession, validateSetupSession } from '../store/stripeSetupSession';
+import { SetupUsage, stripeSetupSession } from '../store/stripeSetupSession';
 import { sitemap } from '../data/sitemap';
-import { ReaderPassport } from '../data/account';
-import { useStripePaySetting } from '../components/hooks/useStripePaySetting';
-import { loadPaymentMethod } from '../repository/stripe';
-import { stripePromise } from '../features/stripepay/StripeContext';
-import { PaySuccessLink } from '../components/text/Checkout';
-import { BankCard } from '../components/BankCard';
+import { useStripeSetup } from '../components/hooks/useStripeSetup';
 
 export function StripeSetupCbPage() {
 
   const { passport } = useAuth();
-  const { selectedPayMethod, setSelectedPayMethod } = useStripePaySetting();
 
   const [ searchParams, _ ] = useSearchParams();
   const navigate = useNavigate();
 
-  const [ progress, setProgress ] = useState(false);
   const [ err, setErr ] = useState('');
 
-  const card = selectedPayMethod?.card;
+  const {
+    progress,
+    onSetupCallback,
+  } = useStripeSetup();
 
   useEffect(() => {
 
@@ -31,92 +27,37 @@ export function StripeSetupCbPage() {
       return;
     }
 
-    const params = newSetupCbParams(searchParams);
-    const sess = stripeSetupSession.load();
+    onSetupCallback(
+        searchParams,
+        passport
+      )
+      .then(usage => {
+        // Navigate back to either checkout page
+        // or stripe setting page.
+        switch (usage) {
+          case SetupUsage.PayNow:
+            navigate(sitemap.checkout);
+            break;
 
-    if (!sess) {
-      setErr('Invalid session');
-      return;
-    }
-
-    const invalid = validateSetupSession({
-      params,
-      sess,
-      passport,
-    });
-
-    if (invalid) {
-      setErr(invalid);
-      return;
-    }
-
-    getPaymentMethod(params.setupIntentClientSecret, passport, sess.usage);
+          case SetupUsage.Future:
+            navigate(sitemap.stripeSetting);
+            break;
+        }
+      })
+      .catch((err: Error) => {
+        setErr(err.message);
+      });
 
     return function clear() {
       stripeSetupSession.clear();
     }
   }, []);
 
-  async function getPaymentMethod(secret: string, pp: ReaderPassport, usage: SetupUsage) {
-
-    const stripe = await stripePromise;
-    if (!stripe) {
-      setErr('Stripe not initialized');
-      return;
-    }
-
-    const result = await stripe.retrieveSetupIntent(secret);
-
-    if (result.error) {
-      setErr(result.error.message || '');
-      return;
-    }
-    const pmId = result.setupIntent.payment_method;
-
-    if (!pmId) {
-      setErr('Setup missing payment method');
-      return;
-    }
-
-    console.log(`Fetching payment method ${pmId}`);
-
-    try {
-      const pm = await loadPaymentMethod(pp.token, pmId);
-
-      setSelectedPayMethod(pm);
-      // If user is setting up payment upon subscription,
-      // navigate back to checkout page.
-      if (usage === SetupUsage.PayNow) {
-        // By now the global stripe payment method
-        // state will have a default payment method
-        // you can display to user on checkout page.
-        navigate(sitemap.checkout);
-      } else {
-        setProgress(false);
-      }
-    } catch (e) {
-      console.log(e);
-      setProgress(false)
-      setErr(err.toString());
-    }
-  }
 
   return (
     <ErrorBoundary errMsg={err}>
       <Loading loading={progress}>
-        <div className="d-flex flex-column align-items-center">
-          <h5>Stripe支付已添加</h5>
-          {
-            card &&
-            <BankCard
-              brand={card.brand}
-              last4={card.last4}
-              expYear={card.expYear}
-              expMonth={card.expMonth}
-            />
-          }
-          <PaySuccessLink/>
-        </div>
+        <div className="text-center">Redidcting...</div>
       </Loading>
     </ErrorBoundary>
   );
