9b1a9c90dbdc1582fe8ba777e1d8828a807a3ad7
weiguo.ni <neefrankie@gmail.com>
2022-06-30 15:12:41 +0800

    refactor

--- src/repository/stripe.ts
+++ src/repository/stripe.ts
@@ -6,109 +6,33 @@ import { Customer, StripePayMethod, SubsResult } from '../data/stripe';
 import { endpoint } from './endpoint';
 import { Fetch, UrlBuilder } from './request';
 
-// Load stripe publishable key from server.
-// It's impossible to load the key dynamically for
-// different environment since it is required to be
-// a fixed static value upon initialization.
-export function loadStripePubKey(): Promise<PubKey> {
-  const url = new UrlBuilder(endpoint.stripePubKey)
-    .toString();
-
-  return new Fetch()
-    .get(url)
-    .endJson<PubKey>()
-}
-
-export function createCustomer(token: string): Promise<Customer> {
-  return new Fetch()
-    .setBearerAuth(token)
-    .post(endpoint.stripeCustomers)
-    .endJson<Customer>();
-}
-
-export function loadCustomer(token: string, cusId: string): Promise<Customer> {
-  const url = new UrlBuilder(endpoint.stripeCustomers)
-    .appendPath(cusId)
-    .toString();
-
-  return new Fetch()
-    .setBearerAuth(token)
-    .get(url)
-    .endJson<Customer>();
-}
-
-export function createSetupIntent(token: string, p: SetupIntentParams): Promise<SetupIntent> {
-
-  return new Fetch()
-    .setBearerAuth(token)
-    .post(endpoint.setupIntent)
-    .sendJson(p)
-    .endJson<SetupIntent>();
-}
-
-export function loadSetupPaymentMethod(token: string, setupId: string): Promise<StripePayMethod> {
-
-  const url = new UrlBuilder(endpoint.setupIntent)
-    .appendPath(setupId)
-    .toString();
-
-  return new Fetch()
-    .setBearerAuth(token)
-    .get(url)
-    .endJson<StripePayMethod>();
-}
-
-export function loadPaymentMethod(token: string, id: string): Promise<StripePayMethod> {
-  const url = new UrlBuilder(endpoint.paymentMethods)
-    .appendPath(id)
-    .toString();
-
-  return new Fetch()
-    .setBearerAuth(token)
-    .get(url)
-    .endJson<StripePayMethod>();
-}
-
-/**
- * @description Load a the default payment method set on customer.
- * @param token
- * @param cusId
- */
- function loadCusDefaultPayMethod(token: string, cusId: string): Promise<StripePayMethod> {
-  const url = new UrlBuilder(endpoint.stripeCustomers)
-    .appendPath(cusId)
-    .appendPath('default-payment-method')
-    .toString();
-
-  return new Fetch()
-    .setBearerAuth(token)
-    .get(url)
-    .endJson<StripePayMethod>();
-}
-
-/**
- * @description Fetches an existing subscription's paymenmt method.
- */
-function loadSubsDefaultPayMethod(
-  token: string,
-  subsId: string,
-): Promise<StripePayMethod> {
-  const url = new UrlBuilder(endpoint.stripeSubs)
-    .appendPath(subsId)
-    .appendPath('default-payment-method')
-    .toString();
-
-  return new Fetch()
-    .setBearerAuth(token)
-    .get(url)
-    .endJson<StripePayMethod>();
-}
-
 type PaymentMethodParams = {
   defaultPaymentMethod: string;
 };
 
 export const stripeRepo = {
+  /**
+   * Load stripe publishable key from server.
+   * It's impossible to load the key dynamically for
+   * different environment since it is required to be
+   * a fixed static value upon initialization.
+   */
+  loadPubKey(): Promise<PubKey> {
+    const url = new UrlBuilder(endpoint.stripePubKey)
+      .toString();
+
+    return new Fetch()
+      .get(url)
+      .endJson<PubKey>()
+  },
+
+  createCustomer(token: string): Promise<Customer> {
+    return new Fetch()
+      .setBearerAuth(token)
+      .post(endpoint.stripeCustomers)
+      .endJson<Customer>();
+  },
+
   /**
    * @description Create a new stripe subscription.
    */
@@ -190,6 +114,15 @@ export const stripeRepo = {
       .endJson<SubsResult>();
   },
 
+  createSetupIntent(token: string, p: SetupIntentParams): Promise<SetupIntent> {
+
+    return new Fetch()
+      .setBearerAuth(token)
+      .post(endpoint.setupIntent)
+      .sendJson(p)
+      .endJson<SetupIntent>();
+  },
+
   /**
    * @description Try to find a stripe default payment method.
    * Use subscription's paymenet method if exists;
@@ -205,6 +138,17 @@ export const stripeRepo = {
     }
 
     return Promise.reject(new Error('Neither stripe subscripiton nor customer found!'));
+  },
+
+  loadPaymentMethod(token: string, id: string): Promise<StripePayMethod> {
+    const url = new UrlBuilder(endpoint.paymentMethods)
+      .appendPath(id)
+      .toString();
+
+    return new Fetch()
+      .setBearerAuth(token)
+      .get(url)
+      .endJson<StripePayMethod>();
   },
 
   /**
@@ -275,4 +219,40 @@ export const stripeRepo = {
       })
       .endJson<Subs>();
   }
+}
+
+
+/**
+ * @description Load a the default payment method set on customer.
+ * @param token
+ * @param cusId
+ */
+ function loadCusDefaultPayMethod(token: string, cusId: string): Promise<StripePayMethod> {
+  const url = new UrlBuilder(endpoint.stripeCustomers)
+    .appendPath(cusId)
+    .appendPath('default-payment-method')
+    .toString();
+
+  return new Fetch()
+    .setBearerAuth(token)
+    .get(url)
+    .endJson<StripePayMethod>();
+}
+
+/**
+ * @description Fetches an existing subscription's paymenmt method.
+ */
+function loadSubsDefaultPayMethod(
+  token: string,
+  subsId: string,
+): Promise<StripePayMethod> {
+  const url = new UrlBuilder(endpoint.stripeSubs)
+    .appendPath(subsId)
+    .appendPath('default-payment-method')
+    .toString();
+
+  return new Fetch()
+    .setBearerAuth(token)
+    .get(url)
+    .endJson<StripePayMethod>();
 }
