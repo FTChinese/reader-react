0b8ec5bb1722909c360d5bcab0b428b30e631fcb
weiguo.ni <neefrankie@gmail.com>
2022-07-06 16:47:32 +0800

    feat: payment methods row

---
+++ src/features/ftcpay/FtcPaymentMethods.tsx
@@ -0,0 +1,52 @@
+import { CheckLarge } from '../../components/graphics/icons';
+import { PaymentKind, paymentMethodIcons } from '../../data/enum';
+
+export function FtcPaymentMethods(
+  props: {
+    selected?: PaymentKind;
+    onSelect: (method: PaymentKind) => void
+  }
+) {
+
+  const oneOffMethods: PaymentKind[] = ['alipay', 'wechat'];
+
+  return (
+    <div className="mb-3">
+      <h6>选择支付方式</h6>
+      {
+        oneOffMethods.map(method =>
+          <OneOffPayMethodItem
+            key={method}
+            method={method}
+            selected={props.selected === method}
+            onClick={() => props.onSelect(method)}
+          />
+        )
+      }
+    </div>
+  );
+}
+
+function OneOffPayMethodItem(
+  props: {
+    method: PaymentKind;
+    selected: boolean;
+    onClick: () => void
+  }
+) {
+  return (
+    <div className="d-flex align-items-center pt-2 pb-2 border-bottom">
+      <div className="flex-grow-1"
+        onClick={props.onClick}
+      >
+        <img src={paymentMethodIcons[props.method]} />
+      </div>
+      <div className="text-teal">
+        {
+          props.selected &&
+          <CheckLarge/>
+        }
+      </div>
+    </div>
+  );
+}
