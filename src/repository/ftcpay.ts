d716e06667047bea38520c2fceec0015091036a4
weiguo.ni <neefrankie@gmail.com>
2022-07-06 17:39:23 +0800

    refactor

--- src/repository/ftcpay.ts
+++ src/repository/ftcpay.ts
@@ -1,36 +1,35 @@
 import { AliPayIntent, ConfirmationResult, WxPayIntent } from '../data/order';
-import { OrderParams } from '../data/shopping-cart';
+import { AliOrderParams, OrderParams } from '../data/shopping-cart';
 import { endpoint } from './endpoint';
 import { Fetch, UrlBuilder } from './request';
 
-export function createWxOrder(v: OrderParams, token: string): Promise<WxPayIntent> {
-  return new Fetch()
-    .setBearerAuth(token)
-    .post(endpoint.wxOrder)
-    .sendJson(v)
-    .endJson<WxPayIntent>();
-}
+export const ftcPayRepo = {
+  createWxOrder(token: string, v: OrderParams): Promise<WxPayIntent> {
+    return new Fetch()
+      .setBearerAuth(token)
+      .post(endpoint.wxOrder)
+      .sendJson(v)
+      .endJson<WxPayIntent>();
+  },
 
-export function createAliOrder(
-  v: OrderParams & { returnUrl: string },
-  token: string
-): Promise<AliPayIntent> {
-  return new Fetch()
-    .setBearerAuth(token)
-    .post(endpoint.aliOrder)
-    .sendJson(v)
-    .endJson<AliPayIntent>();
-}
+  createAliOrder(token: string, v: AliOrderParams): Promise<AliPayIntent> {
+    return new Fetch()
+      .setBearerAuth(token)
+      .post(endpoint.aliOrder)
+      .sendJson(v)
+      .endJson<AliPayIntent>();
+  },
 
-export function verifyAliWxPay(token: string, id: string): Promise<ConfirmationResult> {
-  const url = new UrlBuilder(endpoint.ftcPayBase)
-    .appendPath('orders')
-    .appendPath(id)
-    .appendPath('verify')
-    .toString();
+  verifyAliWxPay(token: string, id: string): Promise<ConfirmationResult> {
+    const url = new UrlBuilder(endpoint.ftcPayBase)
+      .appendPath('orders')
+      .appendPath(id)
+      .appendPath('verify')
+      .toString();
 
-  return new Fetch()
-    .setBearerAuth(token)
-    .post(url)
-    .endJson<ConfirmationResult>();
-}
+    return new Fetch()
+      .setBearerAuth(token)
+      .post(url)
+      .endJson<ConfirmationResult>();
+  },
+};
