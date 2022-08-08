bfb87b0404d6d936bf053c9d3cf597e3ad6f4272
weiguo.ni <neefrankie@gmail.com>
2022-08-08 17:11:12 +0800

    use jsx to build index html

---
+++ scripts/html.dev.tsx
@@ -0,0 +1,41 @@
+import { writeFile } from 'fs/promises';
+import React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { footerMatrix } from './lib/footer';
+import { readPkgFile } from './lib/readPkgFile';
+import { Index } from './template/Index';
+
+const baseUrl = 'https://www.ftacademy.cn/images/favicons';
+const iconSizes = ['180x180', '152x152', '120x120', '76x76']
+
+async function renderIndex(): Promise<string> {
+  const pkg = await readPkgFile();
+
+  const htmlStr = renderToStaticMarkup(
+    <Index
+      bootstrapVersion={pkg.devDependencies.bootstrap}
+      iconBaseUrl={baseUrl}
+      iconSizes={iconSizes}
+      footer={footerMatrix}
+    />
+  );
+
+  return `<!DOCTYPE html>
+  ${htmlStr}
+  `;
+}
+
+renderIndex()
+  .then(content => {
+    return writeFile(
+      'test.html',
+      content,
+      { encoding: 'utf8'}
+    );
+  })
+  .then(() => {
+    console.log('Done');
+  })
+  .catch(err => {
+    console.log(err);
+  });
